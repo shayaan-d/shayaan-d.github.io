1c1dd7a275f675eb5b9bff5b79c24d7e5dabec09
shayaan-d <shayaandoodekula950@gmail.com>
2024-07-14 01:47:15 -0400

    :cat: replaced /static/ with /cat/static so it works

--- cat/static/uv.js
+++ cat/static/uv.js
@@ -1,6 +1,6 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
-importScripts('/static/uv/uv.sw.js');
+importScripts('/cat/static/uv/uv.bundle.js');
+importScripts('/cat/static/uv/uv.config.js');
+importScripts('/cat/static/uv/uv.sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
