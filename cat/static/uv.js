947449028d15950f88813a7e9a8ebcb3498bc4f7
shayaan-d <shayaandoodekula950@gmail.com>
2024-07-14 22:59:45 -0400

    :sob: added ads, sad but has to be done

--- cat/static/uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('/cat/static/uv/uv.bundle.js');
-importScripts('/cat/static/uv/uv.config.js');
-importScripts('/cat/static/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
